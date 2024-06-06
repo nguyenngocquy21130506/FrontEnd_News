41024c53a3b45327a9c743f7bf77c2198b5170e0
Nguyen Ngoc Quy <21130506@st.hcmuaf.edu.vn>
2024-06-07 02:25:55 +0700

    add page login + register

--- news/src/Header.tsx
+++ news/src/Header.tsx
@@ -10,10 +10,11 @@ function Header() {
                 <nav className={styles.nav}>
                     <ul>
                         <li><a href="/">Trang Chủ</a></li>
-                        <li><a href="/tin-tuc">Tin Tức</a></li>
-                        <li><a href="/giai-tri">Giải Trí</a></li>
-                        <li><a href="/the-thao">Thể Thao</a></li>
-                        <li><a href="/lien-he">Liên Hệ</a></li>
+                        <li><a href="/news">Tin Tức</a></li>
+                        <li><a href="/entertainment">Giải Trí</a></li>
+                        <li><a href="/sport">Thể Thao</a></li>
+                        <li><a href="/contact">Liên Hệ</a></li>
+                        <li><a href="/login">Đăng nhập</a></li>
                     </ul>
                 </nav>
             </div>

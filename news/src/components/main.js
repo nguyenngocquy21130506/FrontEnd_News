a6a2c60ed1d05b45bc6e3f628f5b4082adaf0034
QuanghuyDrawn <21130376@st.hcmuaf.edu.vn>
2024-06-13 16:23:05 +0700

    public project

--- news/src/components/main.js
+++ news/src/components/main.js
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom';
 
 function Layout() {
+  const facebookShareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent('http://localhost:3000/')}`;
   return (
     <div>
       <h1>Header</h1>
+      <a href={facebookShareUrl}>Share</a>
       <Outlet />
       <h2>Footer</h2>
     </div>

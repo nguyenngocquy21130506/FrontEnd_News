71c89cf7abb5f7b5148f70a38ebacb2ea986570b
QuanghuyDrawn <21130376@st.hcmuaf.edu.vn>
2024-06-16 20:19:48 +0700

    create category navigation

--- news/src/components/main.js
+++ news/src/components/main.js
@@ -1,16 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import Header from "../Header";
 import Footer from "../Footer";
+import Nav from './nav/Nav';
 
 function Layout() {
   const facebookShareUrl = `https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent('http://localhost:3000/')}`;
   return (
     <div>
-      <h1>Header</h1>
-      <a href={facebookShareUrl}>Share</a>
-      <Header/>
-      <Outlet />
-      <Footer/>
+      <Header />
+      <Nav />
+      {/* <Outlet /> */}
+      <Footer />
     </div>
   );
 }
